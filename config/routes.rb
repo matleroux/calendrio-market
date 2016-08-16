b40d80c1e9bcc4ead330db72adb01c1ceb7db38f
jonathan chevalier <chevalier.jonathan@live.Fr>
2016-08-16 12:53:03 -0300

    adding bookings index and show

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     resources :bookings
   end
 
+  resources :bookings, only: [:index, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+user1 = User.create(email: "chevalier@live.fr", password: "jojojojo")
+user2 = User.create(email: "mat@live.fr", password: "matmat")
+user3 = User.create(email: "roger@live.fr", password: "rogerroger")
+
+place1 = Place.new(name: "lewagon", city: "Paris", country: "france")
+place1.user = user1
+place1.save
+
+place2 = Place.new(name: "plug", city: "Sao Paulo", country: "Brésil" )
+place2.user = user2
+place2.save
+
+place3 = Place.new(name: "maison", city: "marly le roi", country: "France")
+place3.user = user3
+place3.save
+
+booking1 = Booking.new(duration: 11, number_of_participants: 15, status: "pending")
+booking1.place = place1
+booking1.user = user1
+booking1.save
+
+booking2 = Booking.new(duration: 8, number_of_participants: 55, status: "pending")
+booking2.place = place2
+booking2.user = user2
+booking2.save
+
+
