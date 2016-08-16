a5f2280360f3462bb9b9ffd9c7724eddd4f2fb31
Mathieu Le Roux <mathieulr@gmail.com>
2016-08-16 12:54:59 -0300

    create a place for an owner

---
+++ app/controllers/owner/places_controller.rb
@@ -0,0 +1,25 @@
+class Owner::PlacesController < ApplicationController
+  def index
+    @places = current_user.places
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    @place.user = current_user
+    if @place.save
+      redirect_to owner_places_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :address, :city, :country, :max_capacity, :price, :availability, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
     resources :bookings
   end
 
+  namespace :owner do
+    resources :places
+    resources :bookings
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
