ab4f98247bc770c00994503e16980d1b163d3d5a
roger scott <rogerpscott@gmail.com>
2016-08-16 12:59:35 -0300

    add name to user and validate models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,13 @@
 class Booking < ApplicationRecord
   belongs_to :place
   belongs_to :user
+
+  enum status: [:pending, :confirmed, :rejected]
+
+  validates :duration, presence: true
+  validates :number_of_participants, presence: true
+  validates :start_time, presence: true
+  validates :status, presence: true
+  validates :place, presence: true
+  validates :user, presence: true
 end
