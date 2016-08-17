971e654e844c3cbec588615a660abf90520cc426
Mathieu Le Roux <mathieulr@gmail.com>
2016-08-17 14:42:48 -0300

    update delete places

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ApplicationRecord
 
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_attachments :photos, maximum: 5
 
   validates :name, presence: true
