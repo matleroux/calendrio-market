d9e32610972eec6aa59efe5c50c1e4f350ef00b0
roger scott <rogerpscott@gmail.com>
2016-08-17 14:26:16 -0300

    add map to show place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -23,5 +23,10 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     @booking = Booking.new
     @place_coordinates = { latitude: @place.latitude, longitude: @place.longitude }
+    @hash = Gmaps4rails.build_markers([@place]) do |place, marker|
+      marker.lat place.latitude
+      marker.lng place.longitude
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+    end
   end
 end
